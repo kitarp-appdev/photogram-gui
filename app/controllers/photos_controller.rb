27ff5a1388dc6dcb670f441ae1e59c0f30043fd8
kitarp <kitarp@chicagobooth.edu>
2020-11-26 22:18:32 +0000

    first

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,82 @@
+class PhotosController < ApplicationController
+  def index
+    matching_photos = Photo.all
+
+    @list_of_photos = matching_photos.order({ :created_at => :desc })
+
+    render({ :template => "photo_templates/index.html.erb" })
+  end
+
+  def show
+    # Parameters: {"path_id"=>"777"}
+    url_id = params.fetch("path_id")
+
+    matching_photos = Photo.where({ :id => url_id })
+
+    @the_photo = matching_photos.at(0)
+
+    render({ :template => "photo_templates/show.html.erb" })
+  end
+
+  def baii
+    # Parameters: {"toast_id"=>"785"}
+
+    the_id = params.fetch("toast_id")
+
+    matching_photos = Photo.where({ :id => the_id })
+
+    the_photo = matching_photos.at(0)
+
+    the_photo.destroy
+
+    # render({ :template => "photo_templates/baii.html.erb" })
+
+    redirect_to("/photos")
+  end
+
+  def create
+    # Parameters: {"query_image"=>"https://www.chicagobooth.edu/-/media/project/chicago-booth/mba/executive-mba/london-campus/chicago-booth-london-red-buses-street.jpg?cx=0.54&cy=0.42&cw=940&ch=749&hash=521491A15D26AFC14FA3DF729E9F8568", "query_caption"=>"Chicago Booth", "query_owner_id"=>"117"}
+
+    input_image = params.fetch("query_image")
+    input_caption = params.fetch("query_caption")
+    input_owner_id = params.fetch("query_owner_id")
+
+    a_new_photo = Photo.new
+
+    a_new_photo.image = input_image
+    a_new_photo.caption = input_caption
+    a_new_photo.owner_id = input_owner_id
+
+    a_new_photo.save
+
+    # render({ :template => "photo_templates/create.html.erb" })
+
+    next_url = "/photos/" + a_new_photo.id.to_s
+
+    redirect_to(next_url)
+  end
+
+  def update
+    # Parameters: {"query_image"=>"https://www.chicagobooth.edu/-/media/project/chicago-booth/mba/executive-mba/london-campus/chicago-booth-london-red-buses-street.jpg?cx=0.54&cy=0.42&cw=940&ch=749&hash=521491A15D26AFC14FA3DF729E9F8568", "query_caption"=>"Booth Updated", "modify_id"=>"969"}
+
+    the_id = params.fetch("modify_id")
+
+    matching_photos = Photo.where({ :id => the_id })
+
+    the_photo = matching_photos.at(0)
+
+    input_image = params.fetch("query_image")
+    input_caption = params.fetch("query_caption")
+
+    the_photo.image = input_image
+    the_photo.caption = input_caption
+
+    the_photo.save
+
+    # render({ :template => "photo_templates/update.html.erb" })
+
+    next_url = "/photos/" + the_photo.id.to_s
+
+    redirect_to(next_url)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
 
+  get("/", { :controller => "users", :action => "index" })
+
+  get("/users", { :controller => "users", :action => "index" })
+
+  get("/users/:path_username", { :controller => "users", :action => "show" })
+
+  get("/photos", { :controller => "photos", :action => "index" })
+
+  get("/photos/:path_id", { :controller => "photos", :action => "show" })
+
+  get("/delete_photo/:toast_id", { :controller => "photos", :action => "baii" })
+
+  get("/insert_photo", { :controller => "photos", :action => "create" })
+
+  get("/update_photo/:modify_id", { :controller => "photos", :action => "update" })
 
 end
